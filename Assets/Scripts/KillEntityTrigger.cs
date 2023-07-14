b18a16e6a7cbbd958d6f59ae91d0e9b555f298f6
WriteLine <gondilov96@gmail.com>
2023-07-14 15:27:12 +0300

    Update KillEntityTrigger.cs

--- Assets/Scripts/KillEntityTrigger.cs
+++ Assets/Scripts/KillEntityTrigger.cs
@@ -4,6 +4,6 @@ public class KillEntityTrigger : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		collision.gameObject.SetActive(false);
+		Destroy(collision.gameObject);
 	}
 }
